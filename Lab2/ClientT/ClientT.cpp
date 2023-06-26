22ca0360d389e7b90272e31f8c19d66d38367a29
VictorKokos <Travel.ru15@gmail.com>
2023-06-26 20:32:19 +0300

    added many stuff

---
+++ Lab2/ClientT/ClientT.cpp
@@ -0,0 +1,36 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <iostream>
+
+#pragma warning(disable:4996)
+using namespace std;
+int main()
+{
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+	{
+		cout << "Error!" << endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
+
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0)
+	{
+		cout << "Error3!" << endl;
+		return (1);
+	}
+	cout << "Connected!" << endl;
+	char msg[256];
+	recv(Connection, msg, sizeof(msg), NULL);
+	cout << msg << endl;
+
+
+	system("pause");
+}
--- Lab2/ServerT/ServerT.cpp
+++ Lab2/ServerT/ServerT.cpp
@@ -1,9 +1,93 @@
-﻿
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
 #include <iostream>
+
+#pragma warning(disable:4996)
+using namespace std;
+
+
+
+string GetErrorMsgText(int code) // Функция позволяет получить сообщение ошибки
+{
+	string msgText;
+	switch (code)
+	{
+	case WSAEINTR: msgText = "Работа функции прервана";
+		break;
+	case WSAEACCES: msgText = "Разрешение отвергнуто";
+		break;
+	case WSAEFAULT: msgText = "Ошибочный адрес";
+		break;
+	case WSASYSCALLFAILURE: msgText = "Аварийное завершение системного вызова";
+		break;
+	default: msgText = "***ERROR***";
+		break;
+	}
+
+	return msgText;
+}
+string SetErrorMsgText(string msgText, int code)
+{
+	return msgText + GetErrorMsgText(code);
+};
+
+
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	try {
+		WSADATA wsaData;
+		if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+		{
+			throw SetErrorMsgText("startup:", WSAGetLastError());
+		}
+
+
+		SOCKADDR_IN addr;
+		int sizeofaddr = sizeof(addr);
+		addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+		addr.sin_port = htons(1111);
+		addr.sin_family = AF_INET;
+
+		SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+		bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+		listen(sListen, SOMAXCONN);
+
+		SOCKET newConnection;
+		newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+
+		if (newConnection == 0)
+		{
+			throw SetErrorMsgText("socket:", WSAGetLastError());
+		}
+		else
+		{
+			cout << "Client connected!" << endl;
+			char msg[256] = "Message";
+			send(newConnection, msg, sizeof(msg), NULL);
+		}
+
+		system("pause");
+
+
+
+
+
+
+
+		//ЗАКРЫТИЕ
+		if (closesocket(sListen) == SOCKET_ERROR)
+			throw SetErrorMsgText("closesocket:", WSAGetLastError());
+
+		if (WSACleanup() == SOCKET_ERROR)
+			throw SetErrorMsgText("Cleanup:", WSAGetLastError());
+
+	}
+	catch (string errorMsgText)
+	{
+		cout << endl << "WSAGetLastError: " << errorMsgText;
+	}
 }
-
-
